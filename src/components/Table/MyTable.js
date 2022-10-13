6cef5e9551588b3dff69851f72d23917459ccb48
Adam Henderson <adam.henderson@aquaq.co.uk>
2022-10-13 16:45:14 +0100

    Updated endpoint

--- src/components/Table/MyTable.js
+++ src/components/Table/MyTable.js
@@ -6,16 +6,16 @@ export default function MyTable() {
     'ID',
     'First Name',
     'Second Name',
-    'Active',
-    'Salary',
+    'GraduationYear',
     'Edit',
     'Delete'
   ]
 
   const [mainData, setMainData] = useState([])
   const [editingRow, setEditingRow] = useState([])
-  const siteCode = '713a6c1a-a5da-42d6-ae57-58f513ab838d'
-  const myUrl = `https://run.mocky.io/v3/${siteCode}`
+  const siteCode = 'student/'
+  const myUrl = `http://localhost:8080/api/${siteCode}`
+    // https://run.mocky.io/v3/713a6c1a-a5da-42d6-ae57-58f513ab838d
   useEffect(() => {
     if (mainData.length === 0) {
       console.log('I am a use effect hook')
@@ -29,17 +29,16 @@ export default function MyTable() {
   })
   const removeRow = (rowData) => {
     console.log('Removing row: ', rowData)
-    console.log('filter: ', mainData.filter((row) => (row.id === rowData)))
-    setMainData(mainData.filter((row) => (row.id !== rowData)))
+    console.log('filter: ', mainData.filter((row) => (row.studentId === rowData)))
+    setMainData(mainData.filter((row) => (row.studentId !== rowData)))
   }
   const updateRow = (value, rowData, field) => {
-    const rowToUpdate = mainData.filter((row) => (row.id === rowData.id))
+    const rowToUpdate = mainData.filter((row) => (row.studentId === rowData.studentId))
     console.log('value: ', value)
     console.log('field: ', field)
     rowToUpdate[0][field] = value
     console.log(rowToUpdate[0])
   }
-  // https://run.mocky.io/v3/713a6c1a-a5da-42d6-ae57-58f513ab838d
   return (
     <div className="tableDiv">
       <table className="tbl">
@@ -54,10 +53,10 @@ export default function MyTable() {
         </thead>
         <tbody className="tbl-content">
           {mainData.map((data) => (
-            <tr key={data.id}>
+            <tr key={data.studentId}>
               {Object.entries(data).map(([prop, value]) => (
                 <td
-                  contentEditable={data.id === editingRow}
+                  contentEditable={data.studentId === editingRow}
                   field={prop}
                   onBlur={(event) => {
                     updateRow(event.target.innerHTML, data, prop)
@@ -67,12 +66,12 @@ export default function MyTable() {
                 </td>
               ))}
               <td>
-                <button type="button" onClick={() => { setEditingRow(data.id) }}>
+                <button type="button" onClick={() => { setEditingRow(data.studentId) }}>
                   Edit Row
                 </button>
               </td>
               <td>
-                <button type="button" onClick={() => { removeRow(data.id) }}>
+                <button type="button" onClick={() => { removeRow(data.studentId) }}>
                   Delete Row
                 </button>
               </td>
